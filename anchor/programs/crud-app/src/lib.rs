f7c75ab6c5e50bcec10b45edad1628b780ccb9f0
quantmharsh <srivastavah240@gmail.com>
2025-07-21 14:17:45 +0000

    (Feat):Initialized  configuration

---
+++ anchor/programs/crud-app/src/lib.rs
@@ -0,0 +1,14 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS");
+
+//NOTE: Smart contract on solana are Stateless .so All of the states are stored inside
+//the Program Account
+#[program]
+pub mod counter {
+    use super::*;
+
+    
+}
